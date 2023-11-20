d2dcab0f888a8f26c09c7dbf847bfded0379a738
jdvgsa <johnr.dagsa@outlook.com>
2023-11-20 05:24:29 -0700

    debugged contact page, rendered contact section so that when you submit input form it will take you to main page

---
+++ src/lib/components/Contact.svelte
@@ -0,0 +1,32 @@
+<section class="bg-gray-600 py-12 pb-64">
+  <div class="container mx-auto">
+    <h2 class="text-4xl font-bold mb-6 text-center text-white pb-12">Have questions? Fill out the form below to get in touch.</h2>
+
+    <form action="#" method="POST" class="max-w-lg mx-auto">
+      <div class="mb-6">
+        <label for="phone" class="block text-sm font-medium text-white">Phone Number:</label>
+        <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" class="text-black mt-1 p-2 w-full border rounded-md">
+      </div>
+
+      <div class="mb-6">
+        <label for="msg" class="block text-sm font-medium text-white">NSN User:</label>
+        <input type="msg" id="NSN" name="NSN" placeholder="Enter your NSN Username" class="text-black mt-1 p-2 w-full border rounded-md">
+      </div>
+
+      <div class="mb-6">
+        <label for="email" class="block text-sm font-medium text-white">Email Address:</label>
+        <input type="email" id="email" name="email" placeholder="Enter your email address" class="text-black mt-1 p-2 w-full border rounded-md">
+      </div>
+
+      <div class="mb-6">
+        <label for="message" class="block text-sm font-medium text-white">Message:</label>
+        <textarea id="message" name="message" rows="4" placeholder="Enter your message" class="text-black mt-1 p-2 w-full border rounded-md"></textarea>
+      </div>
+
+      <div class="text-center">
+        <a href="/">
+        <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300">Submit</button></a>
+      </div>
+    </form>
+  </div>
+</section>
---
+++ src/routes/contact/[contactslug]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Header from "../../../lib/components/Header.svelte";
+  import Contact from "../../../lib/components/Contact.svelte";
+  import Footer from "../../../lib/components/Footer.svelte";
+</script>
+<Header />
+<Contact />
+<Footer />
