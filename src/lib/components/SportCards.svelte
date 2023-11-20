a0bf4db100d35eac6f76189c02a460aeed5a6ad7
jdvgsa <johnr.dagsa@outlook.com>
2023-11-19 23:43:26 -0700

    added card logos, added card component

---
+++ src/lib/components/SportCards.svelte
@@ -0,0 +1,22 @@
+<section class="flex justify-center justify-evenly pt-5">
+
+  <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10"> 
+    <img class="h-44 w-48" src="/images/NBA.svg" alt="nba-logo">
+    <h3 class= "h5 text-white py-3">National Basketball Association</h3>
+
+    <p class="py-2">For Basketball Enthusiasts.</p>
+  </div>
+
+  <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10"> 
+    <img class="h-44 w-48" src="/images/NHL.svg" alt="nba-logo">
+   <h3 class= "h5 text-white py-3">National Hockey League</h3>
+    <p class="py-2">For Hockey Enthusiasts.</p>
+  </div>
+
+  <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10"> 
+   <img class="h-44 w-48" src="/images/NFL.svg" alt="nba-logo">
+    <h3 class= "h5 text-white py-3">National Football League</h3>
+    <p class="py-2">For Football Enthusiasts.</p>
+  </div>
+
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
 import Header from "../lib/components/Header.svelte";
+import SportCards from "../lib/components/SportCards.svelte";
 </script>
 
 <Header />
+
+<div>
+	<SportCards />
+</div>
