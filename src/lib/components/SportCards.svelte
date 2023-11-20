dba51b157764dbf73eb21bf8a9168d95b4bca1e7
jdvgsa <johnr.dagsa@outlook.com>
2023-11-20 05:30:14 -0700

    made all sports type clickable on home page

--- src/lib/components/SportCards.svelte
+++ src/lib/components/SportCards.svelte
@@ -8,16 +8,20 @@
     </a>
   </div>
 
-  <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10"> 
+  <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10">
+    <a href="/[gallery]"> 
     <img class="h-44 w-48" src="/images/NHL.svg" alt="nba-logo">
    <h3 class= "h5 text-white py-3">National Hockey League</h3>
     <p class="py-2">For Hockey Enthusiasts.</p>
+  </a>
   </div>
 
   <div class="max-w-xs bg-sky-950 hover:bg-violet-500 rounded-3xl p-10"> 
+    <a href="/[gallery]"> 
    <img class="h-44 w-48" src="/images/NFL.svg" alt="nba-logo">
     <h3 class= "h5 text-white py-3">National Football League</h3>
     <p class="py-2">For Football Enthusiasts.</p>
+    <a href="/[gallery]"> 
   </div>
 
 </section>
